eadddb98f0dcdcdce185e00aa94cedefdc7a8cc2
Abhinav Singh <abhinxv18@gmail.com>
2025-06-07 22:48:03 +0530

    day xyz

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,55 +1,117 @@
-import Link from "next/link";
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { AlignJustify, X } from "lucide-react";
 
-const Navbar = () => {
+import Image from "next/image";
+import Link from "next/link";
+import DropDownMenu from "./drop-down-menu";
+
+interface NavbarProps {
+  scrollToWebsiteDesign: () => void;
+  scrollToGraphicDesign: () => void;
+  scrollToShopifyStores: () => void;
+  scrollToBrands: () => void;
+  scrollToServices: () => void; // Define scrollToServices function
+}
+
+const Navbar = ({
+  scrollToWebsiteDesign,
+  scrollToGraphicDesign,
+  scrollToShopifyStores,
+  scrollToBrands,
+  scrollToServices, // Add scrollToServices to props
+}: NavbarProps) => {
+  const [isDropDownVisible, setIsDropDownVisible] = useState(false);
+
+  const toggleDropDown = () => {
+    setIsDropDownVisible(!isDropDownVisible);
+  };
+
+  const closeDropDown = () => {
+    setIsDropDownVisible(false);
+  };
+
   return (
-    <nav className="p-6 md:p-10 flex items-center justify-between z-40 bg-black bg-opacity-10 text-black dark:text-white">
-      <div className="flex items-center gap-3">
-        <Link className="cursor-pointer text-1xl font-bold" href="/">
-          {/* Add your brand name or logo text here if needed */}
-        </Link>
-        <Image
-          priority
-          src="/logo/logo.svg"
-          alt="Logo"
-          width={50}
-          height={50}
-          className="w-10 h-10 md:w-16 md:h-16 rounded-full"
-        />
-      </div>
-      <div className="cursor-pointer hidden md:flex space-x-12 items-center">
-        <Link
-          href="#"
-          className="transition bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 hover:from-blue-400 hover:to-purple-500"
+    <div>
+      <div className="p-6 md:p-10 flex items-center justify-between z-50">
+        <div>
+          <Link className="cursor-pointer" href="/">
+            <Image
+              priority
+              src="/logo/logo.svg"
+              alt="Logo"
+              width={100}
+              height={100}
+              className="w-10 h-10 md:w-14 md:h-14"
+            />
+          </Link>
+        </div>
+        <div
+          className="cursor-pointer hidden 
+            md:flex space-x-10 items-center
+             text-slate-300 text-center 
+             bg-clip-text text-transparent 
+             bg-gradient-to-b from-neutral-50
+              to bg-neutral-400 bg-opacity-50"
         >
-          Website Design
-        </Link>
-        <Link
-          href="#"
-          className="transition bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 hover:from-blue-400 hover:to-purple-500"
-        >
-          Graphic Design
-        </Link>
-        <Link
-          href="#"
-          className="transition bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 hover:from-blue-400 hover:to-purple-500"
-        >
-          Shopify Stores
-        </Link>
-        <Link
-          href="#"
-          className="transition bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 hover:from-blue-400 hover:to-purple-500"
-        >
-          Brands
-        </Link>
-        <Link
-          href="#"
-          className="transition bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 hover:from-blue-400 hover:to-purple-500"
-        >
+          <div onClick={scrollToWebsiteDesign} className="hover:text-gray-50">
+            Website Design
+          </div>
+          <div onClick={scrollToGraphicDesign} className="hover:text-gray-50">
+            Graphic Design
+          </div>
+
+          <div onClick={scrollToShopifyStores} className="hover:text-gray-50">
+            Shopify Stores
+          </div>
+          <div onClick={scrollToBrands} className="hover:text-gray-50">
+            Brands
+          </div>
+
+          <Link href="/pricing" className="hover:text-gray-50">
             Pricing
-        </Link>
+          </Link>
+        </div>
+
+        <div className="flex md:hidden">
+          {isDropDownVisible ? (
+            // display an x icon when the drop is visible
+            <div
+              onClick={toggleDropDown}
+              className="w-8 h-8 text-slate-300 cursor-pointer"
+            >
+              <X />
+              <DropDownMenu
+                onClose={closeDropDown}
+                scrollToServices={scrollToServices} // Pass scrollToServices
+              />
+            </div>
+          ) : (
+            <AlignJustify
+              onClick={toggleDropDown}
+              className="w-8 h-8 text-slate-300 cursor-pointer"
+            />
+          )}
+        </div>
+
+        <div className="hidden md:flex">
+          <Link
+            href="/contact"
+            className="
+            inline-flex h-12 animate-shimmer items-center justify-center 
+            rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] 
+            bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors
+             focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2
+              focus:ring-offset-slate-50
+
+            "
+          >
+            Contact
+          </Link>
+        </div>
       </div>
-    </nav>
+    </div>
   );
 };
 
